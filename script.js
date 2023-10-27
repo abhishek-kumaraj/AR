8774548ded946a2de446dfb033a89d476c09f0ac
abhishek-kumaraj <abhishekkumaraj1506@gmail.com>
2023-10-27 14:50:20 +0530

    modified code

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-var video = document.getElementById('video');
+// Get the XRSession object.
+const session = await navigator.xr.requestSession('immersive-ar');
 
+// Create a XRReferenceSpace object for the real world.
+const referenceSpace = await session.requestReferenceSpace('local');
+
+// Create a XRFrame object.
+const frame = await session.requestAnimationFrame(referenceSpace);
+
+// Get the pose of the camera.
+const cameraPose = frame.getPose(referenceSpace, 'viewer');
+
+// Update the position and rotation of the camera in the scene.
+const camera = document.querySelector('#camera');
+camera.setAttribute('position', cameraPose.position);
+camera.setAttribute('rotation', cameraPose.orientation);
+
+// Start the camera and display the video feed.
+const video = document.getElementById('video');
 navigator.mediaDevices.getUserMedia({ video: true })
   .then(function(stream) {
     video.srcObject = stream;
